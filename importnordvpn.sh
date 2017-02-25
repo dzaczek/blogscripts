dbb21a025ac41421ee5929f1f984369af074b884
dzaczek <sirjacek@gmail.com>
2017-02-25 14:57:22 +0100

    added comments

--- importnordvpn.sh
+++ importnordvpn.sh
@@ -4,11 +4,12 @@
 #author		       :dzaczek consolechars.wordpress.com
 #date            :20170225
 #version         :0.1
-#usage		       :./bash mkscript.sh -u [username] -p [password] -d [directory with ovpn configs]
+#usage           :./bash mkscript.sh -u [username] -p [password] -d [directory with ovpn configs]
 #notes           :Install NetworkManager.x86_64 NetworkManager-openvpn.x86_64 NetworkManager-openvpn-gnome.x86_64 awk
 #notes           : Scprit reqquired time, for add 1583 vpn config needed 3h 2m
 #==============================================================================
 remove_all_vpn(){
+  #remove all vpn utill any vpn conncetion is on a list
   while [[ $(nmcli con show | awk '$3=="vpn" {print "1"}' | wc -l) -gt 0  ]]; do
     nmcli con del $(nmcli con show | awk '$3=="vpn" {print $2}') 2>/dev/null
   done
@@ -28,7 +29,7 @@ while getopts ":u:p:d:c h" opt; do
  esac
 done
 
-
+#check if -h print help end exit
 if [ "x" != "x$ah" ]; then
   cat << EOF
           #script batch adding openvpn  nordvpn configs to nmcli
@@ -57,46 +58,53 @@ EOF
 
   exit 1
 fi
-
+#check if -c if exist remove all vpn
 if [ "x" != "x$ac" ]; then
 
   remove_all_vpn
+#check if username and password id declarated if not exit
 if [ "x" == "x$au" ] && [ "x" == "x$ap" ]; then
   exit 1
 fi
 fi
-
+#checked if username declarated
 if [ "x" == "x$au" ]; then
   echo "-u [username] is required"
   exit 1
 fi
-
+#checked if password is declarated
 if [ "x" == "x$ap" ]; then
   echo "-p [password] is required"
   exit 1
 fi
-
+#checked id direcotry is delcarated
 if [ "x" != "x$ad" ] ; then
 cd $ad 2>/dev/null
+#chek if -d patch is able to cd if not exit
 if [ $? -eq 1 ]; then
   echo "-d $ad wrong patch to directory"
 exit 1
 fi
 fi
-
+#assign varaibles
 USERNAMEFORVPN=$au
 PASSWFORVPN=$ap
+#ssign to vataible a all files *.vpn in directory
 a=$(ls *.ovpn)
-if [[ `echo $a | wc | awk '{print $2}'` -eq 0 ]]; then
+#check   if not len a eq 0
+if [[ `echo $a |wc | awk '{print $2}'` -eq 0 ]]; then
   echo "Ovpn filne in $PWD do not found "
   exit 1
 fi
+#iterating a line by line
  printf '%s\n' "$a" | while IFS= read -r line
 do
+     #prepare short name for connection
      conname=`echo $line | awk  -F "." '{print $1"-"$4}' `
-
+     # add/import connection to nmcli and grap uuid by regex in awk
      uuidcon=$(nmcli connection import type openvpn  file  $line |  awk 'match($0,  /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/) {print substr($0, RSTART, RLENGTH)}')
-    nmcli con mod uuid $uuidcon connection.id $conname +vpn.data "username=$USERNAMEFORVPN" vpn.secrets password="$PASSWFORVPN"
+     #reneme conenction and add username and password
+     nmcli con mod uuid $uuidcon connection.id $conname +vpn.data "username=$USERNAMEFORVPN" vpn.secrets password="$PASSWFORVPN"
      echo "Added conncetion $line , uuid uuid is $uuidcon reneamed to $conname"
 
 done
